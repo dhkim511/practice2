8a0736954079e61be7b7c0ffa89f1f7279a24204
dhkim511 <143858798+dhkim511@users.noreply.github.com>
2024-06-13 11:56:02 +0900

    Update stack.js
    주석 추가

--- stack.js
+++ stack.js
@@ -1,19 +1,25 @@
 class Stack {
   constructor() {
-    this.storage = {};
-    this.count = 0;
+    this.storage = {}; // 스택의 저장소를 객체로 초기화
+    this.count = 0;  // 스택의 크기를 나타내는 count를 0으로 초기화
   }
+  
+  // 스택의 크기를 반환하는 메서드
   size() {
     return this.count;
   }
+  
+  // 스택에 새로운 값을 추가하는 메서드
   push(value) {
-    this.storage[this.count++] = value;
+    this.storage[this.count++] = value; // 현재 count 위치에 값을 저장하고 count를 1 증가시킴
   }
+  
+  // 스택에서 값을 제거하고 반환하는 메서드
   pop() {
-    if (this.count === 0) return undefined;
-    const result = this.storage[--this.count];
-    delete this.storage[this.count];
-    return result;
+    if (this.count === 0) return undefined;// 스택이 비어 있는 경우 undefined를 반환
+    const result = this.storage[--this.count];// count를 1 감소시킨 후 그 위치의 값을 result에 저장
+    delete this.storage[this.count];// 사용한 위치의 값을 삭제
+    return result;// 제거된 값을 반환
   }
 }
 
